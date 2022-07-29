563b6d3204ba2e3091ffcc9f33b1d700be8d6c5c
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-07-29 10:33:40 -0300

    criação dos controller de cada entidade - Rest API - com ecessão do patch de update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlunosModule } from './alunos/alunos.module';
+import { CursosModule } from './cursos/cursos.module';
+import { MatriculasModule } from './matriculas/matriculas.module';
 
 @Module({
-  imports: [],
+  imports: [AlunosModule, CursosModule, MatriculasModule],
   controllers: [AppController],
   providers: [AppService],
 })
