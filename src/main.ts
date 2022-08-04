d9a638e2ec935f968320b8b155bfb3397712d3be
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-04 00:40:05 -0300

    Revert "Merge branch 'add-firebase' into master-nestjs"
    This reverts commit d00bfeb0934f5cdc9244d6a773043da095b0178d, reversing
    changes made to 563b6d3204ba2e3091ffcc9f33b1d700be8d6c5c.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-import { initializeFirebase } from './firebase.config';
 
-dotenv.config();
-initializeFirebase();
-
-async function bootstrap(): Promise<void> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
 bootstrap();
