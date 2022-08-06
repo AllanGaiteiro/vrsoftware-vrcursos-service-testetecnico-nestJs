81a5438d4007d949b4ab56b863ab7f1882283ab3
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-06 17:24:52 -0300

    install and add swagger in boostrap aplication

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -6,6 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix(globalPrefix);
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Vr Cursos')
+    .setVersion('0.0.1')
+    .addTag('cursos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
   await app.listen(3000);
 }
 bootstrap();
