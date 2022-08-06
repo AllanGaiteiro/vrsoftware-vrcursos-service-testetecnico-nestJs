4337091fd436cf2fe3ae722065e5341eaee3a711
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-06 10:05:19 -0300

    add providers of couser and students

--- src/matriculations/matriculations.module.ts
+++ src/matriculations/matriculations.module.ts
@@ -4,10 +4,20 @@ import { MatriculationsService } from './matriculations.service';
 import { MatriculationsController } from './matriculations.controller';
 import { matriculationProviders } from './entities/matriculation.providers';
 import { DatabaseModule } from 'src/database/database.module';
+import { CoursesService } from 'src/courses/courses.service';
+import { StudentsService } from 'src/students/students.service';
+import { courseProviders } from 'src/courses/entities/course.providers';
+import { studentProviders } from 'src/students/entities/student.providers';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [MatriculationsController],
-  providers: [...matriculationProviders, MatriculationsService],
+  providers: [
+    ...matriculationProviders,
+    ...courseProviders, 
+    ...studentProviders,
+    MatriculationsService,
+    CoursesService,
+    StudentsService],
 })
 export class MatriculationsModule {}
