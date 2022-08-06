284746aa67470826039610ebb0d2bd4afaa3219a
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2022-08-06 20:32:51 -0300

    remove inports app.controller and app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { StudentsModule } from './students/students.module';
 import { MatriculationsModule } from './matriculations/matriculations.module';
 
 @Module({
   imports: [CoursesModule, StudentsModule, MatriculationsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
